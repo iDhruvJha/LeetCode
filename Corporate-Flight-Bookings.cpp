3b6c621daf063bbebb6ad61d908f9f7074daa77f
Dhruv Jha <143901402+iDhruvJha@users.noreply.github.com>
2026-06-14 09:47:20 +0530

    [1109] [Time Beats: 32.27%] [Memory Beats: 15.78%] - LeetPush

--- Corporate-Flight-Bookings.cpp
+++ Corporate-Flight-Bookings.cpp
@@ -3,10 +3,14 @@
 3    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
 4        vector<int> ans(n) ;
 5        for(auto it : bookings){
-6            for( int i = it[0] - 1 ; i <= it[1] - 1 ; i++){
-7                ans[i] += it[2] ;
-8            }
-9        }
-10        return ans ;
-11    }
-12};
+6            ans[it[0] - 1] += it[2] ;
+7            if( it[1] < n ){
+8                ans[it[1]] -= it[2] ;
+9            }
+10        }
+11        for(int i = 1 ; i < n ; i++ ){
+12            ans[i] += ans[i-1] ;
+13        }
+14        return ans ;
+15    }
+16};
