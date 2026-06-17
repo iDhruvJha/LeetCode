b468dba6ff1a1c0931dca5af9fd50d72af806a96
Dhruv Jha <143901402+iDhruvJha@users.noreply.github.com>
2026-06-17 10:07:30 +0530

    [802] [Time Beats: 43.13%] [Memory Beats: 35.29%] - LeetPush

--- Find-Eventual-Safe-States.cpp
+++ Find-Eventual-Safe-States.cpp
@@ -1,43 +1,34 @@
 1class Solution {
 2public:
-3    bool dfs(int node , vector<vector<int>>& adj ,
-4             vector<int>& vis , vector<int>& path ,vector<int>& check){
-5
-6        vis[node] = 1;
-7        path[node] = 1;
-8
-9        for(auto it : adj[node]){
-10            if(!vis[it]){
-11                if(dfs(it,adj,vis,path,check)){
-12                    check[it] = 1 ;
-13                    return true ;
-14                }
-15            }
-16            else if(path[it]){
-17                check[it] = 1 ;
-18                return true;
-19            }
-20        }
-21
-22        path[node] = 0;
-23        return false;
-24    }
-25    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-26        int V = graph.size() ;
-27        vector<int> vis(V,0);
-28        vector<int> path(V,0);
-29        vector<int> check(V,0) ;
-30        for(int i = 0 ; i < V ; i++){
-31            if(!vis[i]){
-32                if(dfs(i,graph,vis,path,check)){
-33                    check[i] = 1 ;
-34                }
-35            }
-36        }
-37        vector<int> s ;
-38        for(int i = 0 ; i < V ; i++ ){
-39            if(!check[i]) s.push_back(i) ;
-40        }
-41        return s;
-42    }
-43};
+3    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+4        int n = graph.size();
+5        vector<int> indegree(n,0) ;
+6        vector<vector<int>> adj(n);
+7        for (int i = 0 ; i < n ; i++ ) {
+8            for(auto it : graph[i]){
+9                adj[it].push_back(i) ;
+10                indegree[i]++ ;
+11            }
+12        }
+13        queue<int> q  ;
+14        for (int i = 0; i < n; i++) {
+15            if (indegree[i] == 0) {
+16                q.push(i);
+17            }
+18        }
+19        vector<int> topo;
+20        while (!q.empty()) {
+21            int node = q.front();
+22            q.pop();
+23            topo.push_back(node);
+24            for (auto it : adj[node]) {
+25                indegree[it]--;
+26                if (indegree[it] == 0) {
+27                    q.push(it);
+28                }
+29            }
+30        }
+31        sort(topo.begin(),topo.end()) ;
+32        return topo ;
+33    }
+34};
