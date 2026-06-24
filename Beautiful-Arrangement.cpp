54dcab6bf61c472678bf116c221889b5ae831bde
Dhruv Jha <143901402+iDhruvJha@users.noreply.github.com>
2026-06-24 07:56:02 +0530

    [526] [Time Beats: 73.54%] [Memory Beats: 99.56%] - LeetPush

--- Beautiful-Arrangement.cpp
+++ Beautiful-Arrangement.cpp
@@ -1,20 +1,21 @@
 1class Solution {
 2public:
-3    bool seen[16] = {} ;
+3    int seen = 0 ;
 4    int count = 0 ;
 5    int dfs(int n , int pos){
 6        if( pos > n ) return count++ ;
 7        for( int i = 1 ; i <= n ; i++ ){
-8            if(!seen[i] && ( (pos % i == 0) || ( i % pos == 0 ))){
-9                seen[i] = true ;
-10                dfs(n,pos+1) ;
-11                seen[i] = false ;
-12            }
-13        }
-14        return count ;
-15    }
-16    int countArrangement(int n) {
-17        if( n < 4 ) return n ;
-18        return dfs(n,1) ;
-19    }
-20};
+8            int bit = 1 << i ;
+9            if(!(seen & bit) && ( (pos % i == 0) || ( i % pos == 0 ))){
+10                seen ^= bit ;
+11                dfs(n,pos+1) ;
+12                seen ^= bit ;
+13            }
+14        }
+15        return count ;
+16    }
+17    int countArrangement(int n) {
+18        if( n < 4 ) return n ;
+19        return dfs(n,1) ;
+20    }
+21};
