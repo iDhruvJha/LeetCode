3e1b6e02d797b2e1e72bdc384e69d2b15e99e90f
Dhruv Jha <143901402+iDhruvJha@users.noreply.github.com>
2026-06-25 13:50:26 +0530

    [10] [Time Beats: 27.23%] [Memory Beats: 13.65%] - LeetPush

--- Regular-Expression-Matching.cpp
+++ Regular-Expression-Matching.cpp
@@ -1,6 +1,6 @@
 1class Solution {
 2public:
-3    bool f(string &s,string &p,int i,int j){
+3    bool f(string &s,string &p,int i,int j ,vector<vector<int>>& dp){
 4
 5        int n=s.size();
 6        int m=p.size();
@@ -10,30 +10,35 @@
 10
 11        if(j==m)
 12            return false;
-13
-14        if(i==n){
-15            while(j+1<m && p[j+1]=='*')
-16                j+=2;
-17
-18            return j==m;
-19        }
-20
-21        bool match=(s[i]==p[j] || p[j]=='.');
+13        
+14        if(dp[i][j] != -1) return dp[i][j] ;
+15
+16        if(i==n){
+17            while(j+1<m && p[j+1]=='*')
+18                j+=2;
+19
+20            return dp[i][j] = j==m;
+21        }
 22
-23        if(j+1<m && p[j+1]=='*'){
+23        bool match=(s[i]==p[j] || p[j]=='.');
 24
-25            return f(s,p,i,j+2) ||
-26                  (match && f(s,p,i+1,j));
-27
-28        }
+25        if(j+1<m && p[j+1]=='*'){
+26
+27            return dp[i][j] = f(s,p,i,j+2,dp) ||
+28                  (match && f(s,p,i+1,j,dp));
 29
-30        if(match)
-31            return f(s,p,i+1,j+1);
-32
-33        return false;
-34    }
-35
-36    bool isMatch(string s, string p) {
-37        return f(s,p,0,0);
-38    }
-39};
+30        }
+31
+32        if(match)
+33            return dp[i][j] = f(s,p,i+1,j+1,dp);
+34
+35        return dp[i][j] = false;
+36    }
+37
+38    bool isMatch(string s, string p) {
+39        int n=s.size();
+40        int m=p.size();
+41        vector<vector<int>> dp(n+1,vector<int>(m+1,-1)) ;
+42        return f(s,p,0,0,dp);
+43    }
+44};
