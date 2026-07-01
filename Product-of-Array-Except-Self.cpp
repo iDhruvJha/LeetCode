7fedb370039d264b569d262bd71eff794bc1b6f8
Dhruv Jha <143901402+iDhruvJha@users.noreply.github.com>
2026-07-01 16:21:42 +0530

    [238] [Time Beats: 100.00%] [Memory Beats: 60.92%] - LeetPush

--- Product-of-Array-Except-Self.cpp
+++ Product-of-Array-Except-Self.cpp
@@ -2,27 +2,28 @@
 2public:
 3    vector<int> productExceptSelf(vector<int>& nums) {
 4        int n = nums.size() ;
-5        long long tp = 1 ;
-6        bool ch = false ;
-7        int ind = -1 ;
-8        for(int i = 0 ; i < n ; i++ ){
-9            if(nums[i] != 0 || ch ){
-10                tp *= nums[i] ;
-11            }
-12            else{
-13                ch = true ;
+5        int zeros = 0 ;
+6        int product = 1 ;
+7        vector<int> ans(n,0) ; int ind = -1 ;
+8        for( int i = 0 ; i < n ; i++ ){
+9            if( nums[i] == 0 ){
+10                zeros++ ;
+11                if( zeros > 1 ){
+12                    return ans ;
+13                }
 14                ind = i ;
 15            }
-16        }
-17        vector<int> ans(n,0) ;
-18        if(!tp && ch) return ans ;
-19        if(ch){
-20            ans[ind] = tp ;
-21            return ans ;
-22        }
-23        for(int i = 0 ; i < n ; i++ ){
-24            ans[i] = tp/nums[i] ;
-25        }
-26        return ans ;
-27    }
-28};
+16            else{
+17                product *= nums[i] ;
+18            }
+19        }
+20        if( zeros == 1 ){
+21            ans[ind] = product ;
+22            return ans ;
+23        }
+24        for( int i = 0 ; i < n ; i++ ){
+25            ans[i] = product/nums[i] ;
+26        }
+27        return ans ;
+28    }
+29};
